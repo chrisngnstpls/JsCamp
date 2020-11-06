2325f49f1d00cc09d52ab121bb95db8714a2ec23
chrisngnstpls <chrisngnstpls@gmail.com>
2020-11-06 20:18:24 +0200

    Class exercise

--- 06112020.js
+++ 06112020.js
@@ -1,23 +1,27 @@
+/* Class exercises : 
+    do while loops
+    while loops
+    ternary if statement
+*/
+
+
+
 // var x=8;
 // k=(x==5)?"It is five":"It is not five";
 // console.log(k);
 
+
 // var x = 1280;
 // var y = 1280;
-
 // function decide(width, height){
 //     let msg;
-    
-//     if (width==height){
+    //     if (width==height){
 //             msg="It is a square";
 //     } else {
 //         msg=(width>height)?"It is landscape":"It is portrait";
 //     }
 //     return msg;
 // }
-
-
-
 // result = decide(x,y);
 // console.log(result);
 
@@ -37,19 +41,77 @@ console.log(x);
 */
 
 // const arr = [1,2,3,4,5,6,7]
-
 // while (arr.length>0) {
 //     const elem = arr.shift();
 //     console.log(elem);
 // }
 
-function exerciseDo(numberOfTimes){
-    let i = 0;
+// function exerciseDo(numberOfTimes){
+//     let i = 0;
+//     do{
+//         i++
+//         console.log(i)
+//     } while (i<numberOfTimes)
+// }
+// exerciseDo(7)
 
-    do{
-        i++
-        console.log(i)
-    } while (i<numberOfTimes)
+// e = []
+// e[20] = 'ad';
+// console.log(e);
+// console.log(e.length);
+// console.log(typeof e[4]);
+
+
+function exerciseDate (){
+    date = new Date();
+    month = date.getMonth();
+    let currentMonth;
+    switch(month){
+        case 0:
+            currentMonth = "January";
+            break;
+        case 1:
+            currentMonth = "February";
+            break;
+        case 2:
+            currentMonth = "March";
+            break;
+        case 3:
+            currentMonth = "April";
+            break;
+        case 4:
+            currentMonth = "May";
+            break;
+        case 5:
+            currentMonth = "June";
+            break;
+        case 6:
+            currentMonth = "July";
+            break;
+        case 7:
+            currentMonth = "August";
+            break;
+        case 8:
+            currentMonth = "September";
+            break;
+        case 9:
+            currentMonth = "October";
+            break;
+        case 10:
+            currentMonth = "November";
+            break;
+        case 11:
+            currentMonth = "December";
+            break;             break;
+        default:
+            console.log('Something went wrong');
+            break;    
+
+    }
+
+
+
+    console.log(currentMonth);
 }
 
-exerciseDo(7)
+exerciseDate()

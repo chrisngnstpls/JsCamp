359eac8a3cacda244effd367777d0360677c02a4
chrisngnstpls <chrisngnstpls@gmail.com>
2020-11-06 19:50:56 +0200

    Class Exercise

--- 06112020.js
+++ 06112020.js
@@ -2,21 +2,54 @@
 // k=(x==5)?"It is five":"It is not five";
 // console.log(k);
 
-var x = 1280;
-var y = 1280;
+// var x = 1280;
+// var y = 1280;
 
-function decide(width, height){
-    let msg;
+// function decide(width, height){
+//     let msg;
     
-    if (width==height){
-            msg="It is a square";
-    } else {
-        msg=(width>height)?"It is landscape":"It is portrait";
-    }
-    return msg;
+//     if (width==height){
+//             msg="It is a square";
+//     } else {
+//         msg=(width>height)?"It is landscape":"It is portrait";
+//     }
+//     return msg;
+// }
+
+
+
+// result = decide(x,y);
+// console.log(result);
+
+
+/*
+***This does not run***
+***returns : SyntaxError: Unexpected token '?' 
+
+
+let a = null;
+let b = undefined;
+let c = 'something';
+
+x = (a??b??c);
+console.log(x);
+
+*/
+
+// const arr = [1,2,3,4,5,6,7]
+
+// while (arr.length>0) {
+//     const elem = arr.shift();
+//     console.log(elem);
+// }
+
+function exerciseDo(numberOfTimes){
+    let i = 0;
+
+    do{
+        i++
+        console.log(i)
+    } while (i<numberOfTimes)
 }
 
-
-
-result = decide(x,y);
-console.log(result);
+exerciseDo(7)

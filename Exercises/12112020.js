d4c075c0cf14e4ccc3fcf99108b770ba59a34257
chrisngnstpls <chrisngnstpls@gmail.com>
2020-11-12 18:36:58 +0200

    update

--- Exercises/12112020.js
+++ Exercises/12112020.js
@@ -4,6 +4,7 @@ Error handling
 
 
 */
+/*
     function arrayFromRange (min,max){
         let myArray=[];
         console.log('starting');
@@ -16,3 +17,23 @@ Error handling
 
     x = arrayFromRange(5,15);
     console.log(x);
+
+*/
+// second method of solving
+
+    function anotherAttempt (min, max){
+        let myArray=[];
+        let i=min;
+        console.log('starting');
+        
+        while (i<max){
+            i++
+            pushValue = i+1;
+            myArray.push(pushValue);
+
+        }
+        return myArray;
+    }
+
+    f = anotherAttempt(5,10);
+    console.log(f);
